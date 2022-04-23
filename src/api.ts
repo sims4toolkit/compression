7f9535f68d3180510392fb1fe76c0cb1a3d5cbbf
Frank Kulak <fs.kulak@gmail.com>
2022-04-23 15:44:08 -0400

    Add CompressedBuffer interface.

--- src/api.ts
+++ src/api.ts
@@ -1,8 +1,10 @@
+import CompressedBuffer from "./compressed-buffer";
 import CompressionType from "./compression-type";
 import compressBuffer from "./compress";
 import decompressBuffer from "./decompress";
 
 export {
+  CompressedBuffer,
   CompressionType,
   compressBuffer,
   decompressBuffer
